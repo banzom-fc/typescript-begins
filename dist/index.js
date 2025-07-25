3610177d8eee2532e15f69d070d78bfee4739998
banzom-fc <aditya.01.in@freecharge.in>
2025-07-25 11:10:32 +0530

    Enhance TypeScript examples with boolean and any type demonstrations

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,15 @@
 "use strict";
 console.log(Math.floor(3.4));
-var myNumber = 5;
+var myNumber = 42;
 console.log(myNumber);
 var myString = "Hello, TypeScript!";
 console.log(myString);
+// Another Way
+var myBoolean;
+myBoolean = true;
+console.log(myBoolean);
+// Any type:
+var myAny = "This can be anything";
+console.log(myAny);
+myAny = 100; // Now it's a number
+console.log(myAny);
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,16 @@ console.log(myNumber);
 
 var myString: string = "Hello, TypeScript!";
 console.log(myString);
+
+// Another Way
+var myBoolean: boolean;
+
+myBoolean = true;
+console.log(myBoolean);
+
+
+// Any type:
+var myAny: any = "This can be anything";
+console.log(myAny);
+myAny = 100; // Now it's a number
+console.log(myAny);
